b7590cd09ff526db89993b67bd67f6ab3036cbae
Box <bradycfox@gmail.com>
2018-11-07 15:43:34 -0500

    infoOutput

--- src/components/infoOutput.js
+++ src/components/infoOutput.js
@@ -1,1 +1,28 @@
 import React from 'react';
+
+export default function InfoOutput(props) {
+  let location = props.zipInfo[0];
+  if(props.localWeather[0]) {
+  return (
+    <div>
+      <p>Here is the information your request on zipcode: {location.Zipcode}</p>
+      <ul>
+        <li> Longitude: {location.Longitude}</li>
+        <li>Latitude: {location.Latitude}</li>
+        <li>City: {location.City}</li>
+        <li>State: {location.State}</li>
+        <li>County: {location.County}</li>
+      </ul>
+    </div>
+  )
+  }
+
+  else {
+    return (
+      <div>
+        <p>Please enter a California Zipcode to learn more!</p>
+      </div>
+
+    )
+  }
+}
